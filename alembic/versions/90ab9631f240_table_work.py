89fc38b62f6726e7c682028991d4a252eb9a6974
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-09-30 13:06:07 -0300

    fix: running make pre-commit

--- alembic/versions/90ab9631f240_table_work.py
+++ alembic/versions/90ab9631f240_table_work.py
@@ -1,13 +1,13 @@
 """table_work
 
 Revision ID: 90ab9631f240
-Revises: 
+Revises:
 Create Date: 2022-09-30 12:53:16.433223
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '90ab9631f240'
