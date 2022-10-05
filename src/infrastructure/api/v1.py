04d808096a2214ac0a2467cf67a2fe7f6bd93053
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-10-04 22:37:37 -0300

    feat: added router health-check

--- src/infrastructure/api/v1.py
+++ src/infrastructure/api/v1.py
@@ -1,3 +1,6 @@
 from fastapi.routing import APIRouter
 
+from src.infrastructure.api.routers.health_check import health_router
+
 router = APIRouter(prefix="/api/v1")
+router.include_router(router=health_router)
