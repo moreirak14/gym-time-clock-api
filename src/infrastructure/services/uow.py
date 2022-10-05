fbc89e3ea35255a03cd2d1329132a0df95c42209
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-10-04 22:16:04 -0300

    feat: added Unit of Work Uses SQLAlchemy Sessions

---
+++ src/infrastructure/services/uow.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from src.adapters.databases import Session
+
+from src.commons.abstracts.unity_of_work import AbstractUnitOfWork
+
+DEFAULT_SESSION_FACTORY = Session
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    default_session_factory = DEFAULT_SESSION_FACTORY
+
+    def __init__(self, session_factory=None):
+        self.session_factory = (
+            session_factory
+            if session_factory is not None
+            else self.default_session_factory
+        )
+
+    def __enter__(self):
+        self.session = self.session_factory()
+
+    def __exit__(self, *args):
+        self.rollback()
+        self.session.close()
+
+    def _commit(self):
+        self.session.commit()
+
+    def _flush(self):
+        self.session.flush()
+
+    def rollback(self):
+        self.session.rollback()
