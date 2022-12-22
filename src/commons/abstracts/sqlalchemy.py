57a9aca628d638be768746a082ffe108b225dac1
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-12-21 22:03:32 -0300

    feat: added work repository in sql UOW

--- src/commons/abstracts/sqlalchemy.py
+++ src/commons/abstracts/sqlalchemy.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 from src.adapters.databases import Session
+from src.adapters.repositories.work import WorkRepository
 from src.commons.abstracts.unity_of_work import AbstractUnitOfWork
+from src.domain.work.entity.work import Work
 
 DEFAULT_SESSION_FACTORY = Session
 
 
 class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
     default_session_factory = DEFAULT_SESSION_FACTORY
+
+    work: WorkRepository[Work]
 
     def __init__(self, session_factory=None):
         self.session_factory = (
@@ -18,6 +22,7 @@ class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
 
     def __enter__(self):
         self.session = self.session_factory()
+        self.work = WorkRepository(self.session, Work)
 
     def __exit__(self, *args):
         self.rollback()
